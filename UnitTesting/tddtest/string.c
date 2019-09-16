fe6822c95964f3d3f2e8341b8e12644d75f378d5
quynhhgoogoo <luongdiemquynh1998@gmail.com>
2019-09-17 00:20:53 +0300

    fix

--- UnitTesting/tddtest/string.c
+++ UnitTesting/tddtest/string.c
@@ -5,7 +5,7 @@
 /*return length of string*/
 int my_strlen(char *s){
 	int len=0;
-	while(len!='\0'){
+	while(*(s+len) != '\0'){
 		len++;
 	}
 	return len;
@@ -13,54 +13,51 @@ int my_strlen(char *s){
 
 /*return number of uppercase character*/
 int str2upper(char *s){
-	int c = 0;
-	int num =0;
-	while(c!='\0'){
-		if(*(s+c)>='a' && *(s+c)<='z'){
-			*(s+c)=*(s+c)-32;
-			num++;
+	int i = 0,count =0;
+	while(*(s+i)!='\0'){
+		if(*(s+i)>='a' && *(s+i)<='z'){
+			*(s+i)=*(s+i)-32;
+			count++;
 		}
-		c++;
+		i++;
 	}
-	return num;
+	return count;
 }
 
 /*return number of lowercase character*/
 int str2lower(char *s){
-	int c =0;
-	int num = 0;
-	while(*(s+c)!='\0'){
-		if(*(s+c) >= 'A' && *(s+c)<='Z'){
-			*(s+c)=*(s+c)+32;
-			num++;
+	int i =0, count = 0;
+	while(*(s+i)!='\0'){
+		if(*(s+i) >= 'A' && *(s+i)<='Z'){
+			*(s+i)=*(s+i)+32;
+			count++;
 		}
-		c++;
+		i++;
 	}
+	return count;
 }
 
 /*remove occurence number and return new string length*/
-int str_strip_number(char *s){
-	int i = 0;
-	int len = my_strlen(s);
-	while(*(s+i)!='\0'){
-		if(*(s+i)>=0 && *(s+i)<=9){
-			i++;
-			while(i<len){
-				*(s+i)=*(s+i+1);
-			}
-		}
-		return i;
-	}
+int str_strip_numbers( char *s){
+        int j=0,k;
+        for(int i=0; s[i]; i++){
+                        if(s[i] <= '0' || s[i] >= '9'){
+                                s[j] = s[i];
+                                j++;
+                        }
+        }
+        s[j]='\0';
+        return j;
 }
 
 /*copy two strings together*/
 void mystrcpy(char *s, char *d){
-	while(*s){
-		*d = *s;
-		d++;
-		s++;
+	int i=0;
+	while(*(s+i)!='\0'){
+		*(d+i) = *(s+i);
+		i++;
 	}
-	*d='\0';
+	*(d+i)='\0';
 }
 
 /*compare two strings together and returns place of first different character*/
@@ -73,29 +70,24 @@ int mystrcmp(char*s, char *d){
 			break;
 		}
 	}
-	if(*(s+i)==*(d+i))
-		return 0;
+	if(*(s+i)>*(d+i))
+		return 1;
 	if(*(s+i)<*(d+i))
 		return -1;
 	else
-		return 1;
+		return 0;
 
 }
 
 /*creaes duplicated string*/
 char *strdupl(char *s){
+	int i =0;
 	char *d = (char*)malloc(sizeof(char*)*100);
-	while(*s){
-		*d = *s;
-		d++;
-		s++;
+	while(*(s+i)!='\0'){
+		*(d+i) = *(s+i);
+		i++;
 	}
-	*d = '\0';
+	*(d+i) = '\0';
+
+	return d;
 }
-
-
-
-
-
-
-
