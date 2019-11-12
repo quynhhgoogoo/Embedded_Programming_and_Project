332d763e4c4058aafaaea1b0b3e6a70ae7733846
quynhhgoogoo <luongdiemquynh1998@gmail.com>
2019-11-12 20:49:57 +0200

    linkedlist ini set up

---
+++ linkedlist/linkedlist.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "linkedlist.h"
+
+int add_to_list(linked_list *ll, char *s){
+	if(ll == NULL){
+	}
+}
+
+int display_item(linked_list *ll){}
+
+int display_list(linked_list *ll){}
+
+linked_list *search_from_list(linked_list *ll, char *s){}
+
+int delete_from_list(linked_list *ll, int index){}
---
+++ linkedlist/linkedlist.h
@@ -0,0 +1,22 @@
+typedef struct linked_list linked_list;
+
+struct linked_list{
+	char *data;
+	int index;	/*position of the element in the list*/
+	struct linked_list *next;
+};
+
+/*create new list item and add to the end of the list*/
+int add_to_list(linked_list *ll, char *s);
+
+/*display required element in a list*/
+int display_item(linked_list *ll);
+
+/*list all elements in a list*/
+int display_list(linked_list *ll);
+
+/*search for data *s from a list*/
+linked_list *search_from_list(linked_list, char *s);
+
+/*delete item from a list and free space*/
+int delete_from_list(linked_list *ll, int index);
